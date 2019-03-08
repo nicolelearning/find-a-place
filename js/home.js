aedd46cd18f1909da5d27ef64aa2882b1b5295cf
klaus <oriokiklaus@gmail.com>
2019-03-08 11:30:59 +0300

    add relevant pictures to home.js

--- js/home.js
+++ js/home.js
@@ -1,59 +1,61 @@
 //businna
 //adventure
 var slideimages = [
-  "https://media.nature.com/w1172/magazine-assets/d41586-017-08492-y/d41586-017-08492-y_15320844.jpg",
-  "https://www.standardmedia.co.ke/images/friday/thumb_lqto7hyjun9hezsfkn5aa2e281a314f.jpg",
-  "https://2.bp.blogspot.com/-T0bnMWIN-gw/WcOlyaHdjOI/AAAAAAAAAug/rx2ZA_xwpdAu5sxnhr9Uyl-3eStcjgA6wCLcBGAs/s1600/ihub-room.jpg",
-  "https://i.ytimg.com/vi/eoEVq9M30R0/maxresdefault.jpg",
+  "https://eatout2.scdn5.secure.raxcdn.com/images/restaurant_gallery/6054-728-1524494338.jpg",
+  "https://hangout.co.ke/wp-content/uploads/2014/05/48126_615645555115832_556441404_n.jpg",
+  "https://naibuzz.com/wp-content/uploads/2016/07/Cmv79IvWIAA5Pyh.jpg",
+  "https://cdn.shopify.com/s/files/1/0709/9055/files/blog_alpine_initiatives_090701_3_low.jpg?3844080310051031636",
 ];
 var placenames = [
-  "sun",
-  "mtkenya",
-  "ihub",
-  "longonot",
+  " Restaurants",
+  " Art",
+  " Gaming",
+  " Wildlife",
 ];
 //Art
-var artImages = [  "https://media.nature.com/w1172/magazine-assets/d41586-017-08492-y/d41586-017-08492-y_15320844.jpg",
-  "https://www.standardmedia.co.ke/images/friday/thumb_lqto7hyjun9hezsfkn5aa2e281a314f.jpg",
-  "https://2.bp.blogspot.com/-T0bnMWIN-gw/WcOlyaHdjOI/AAAAAAAAAug/rx2ZA_xwpdAu5sxnhr9Uyl-3eStcjgA6wCLcBGAs/s1600/ihub-room.jpg",
-  "https://i.ytimg.com/vi/eoEVq9M30R0/maxresdefault.jpg",];
-var artplacenames= ["sun",
-"mtkenya",
-"ihub",
-"longonot",];
+var artImages = [  "https://hangout.co.ke/wp-content/uploads/2014/05/48126_615645555115832_556441404_n.jpg",
+  "https://3dwnh01icn0h133s00sokwo1-wpengine.netdna-ssl.com/wp-content/uploads/2015/11/Screen-Shot-2015-11-15-at-10.08.21-AM.jpg",
+  "https://i2.wp.com/www.momoafrica.com/wp-content/uploads/2017/06/Galerie-El-Marsa.jpg?resize=1320%2C880&ssl=1",
+  "http://www.malindikenya.net/images/uploads/articoli/1411_l.jpg",];
+var artplacenames= [
+" Art gallery in Nairobi",
+" Sarit center art fair",
+" Banana hill ",
+" Osteria in malindi"];
 
 //Eateries
-var eateries = [  "https://media.nature.com/w1172/magazine-assets/d41586-017-08492-y/d41586-017-08492-y_15320844.jpg",
-  "https://www.standardmedia.co.ke/images/friday/thumb_lqto7hyjun9hezsfkn5aa2e281a314f.jpg",
-  "https://2.bp.blogspot.com/-T0bnMWIN-gw/WcOlyaHdjOI/AAAAAAAAAug/rx2ZA_xwpdAu5sxnhr9Uyl-3eStcjgA6wCLcBGAs/s1600/ihub-room.jpg",
-  "https://i.ytimg.com/vi/eoEVq9M30R0/maxresdefault.jpg",];
-var eateriesplacenames= ["sun",
-"mtkenya",
-"ihub",
-"longonot",];
+var eateries = [  "https://eatout2.scdn5.secure.raxcdn.com/images/restaurant_gallery/6054-728-1524494338.jpg",
+  "http://www.travelstart.co.ke/blog/wp-content/uploads/2014/05/Tamambo-Blixen.jpg",
+  "http://www.travelstart.co.ke/blog/wp-content/uploads/2014/05/Seven-SeaFood-Restaurant.jpg",
+  "http://www.travelstart.co.ke/blog/wp-content/uploads/2014/05/Asmara-Restaurant.jpg",];
+var eateriesplacenames= ["Baluba restaurant westlands",
+" Tamambo blixen, Karen",
+" Seven seafood",
+"Asmara, westlands",];
 
 
 //History
 var historyy = [
-  "https://media.nature.com/w1172/magazine-assets/d41586-017-08492-y/d41586-017-08492-y_15320844.jpg",
-  "https://www.standardmedia.co.ke/images/friday/thumb_lqto7hyjun9hezsfkn5aa2e281a314f.jpg",
-  "https://2.bp.blogspot.com/-T0bnMWIN-gw/WcOlyaHdjOI/AAAAAAAAAug/rx2ZA_xwpdAu5sxnhr9Uyl-3eStcjgA6wCLcBGAs/s1600/ihub-room.jpg",
+  "http://s.embark.org/0.9.0/media/820x400/Kenya/images/Mombasa/fort-jesus-mombasa-kenya.jpg",
+  "http://www.magicalkenya.com/wp-content/uploads/2014/08/gederuinsimg1.jpg",
+  "https://africanahgirl.files.wordpress.com/2015/09/image8.jpg",
   "https://i.ytimg.com/vi/eoEVq9M30R0/maxresdefault.jpg",
 ];
-var historyplacenames = ["sun",
-"mtkenya",
-"ihub",
-"longonot",];
+var historyplacenames = [" Fort Jesus, Mombasa",
+" Gedi ruins",
+" KICC building, Nairobi",
+" The longonot",];
 
 //Gaming
-var gaming = [  "https://media.nature.com/w1172/magazine-assets/d41586-017-08492-y/d41586-017-08492-y_15320844.jpg",
-  "https://www.standardmedia.co.ke/images/friday/thumb_lqto7hyjun9hezsfkn5aa2e281a314f.jpg",
-  "https://2.bp.blogspot.com/-T0bnMWIN-gw/WcOlyaHdjOI/AAAAAAAAAug/rx2ZA_xwpdAu5sxnhr9Uyl-3eStcjgA6wCLcBGAs/s1600/ihub-room.jpg",
-  "https://i.ytimg.com/vi/eoEVq9M30R0/maxresdefault.jpg",];
-var gamingplacenames = ["sun",
-"mtkenya",
-"ihub",
-"longonot",];
+var gaming = [  "https://naibuzz.com/wp-content/uploads/2016/07/Cmv79IvWIAA5Pyh.jpg",
+  "https://www.sde.co.ke/sdemedia/sdeimages/friday/gdohkankhc8e2sh5599ff3a081d8f.jpg",
+  "https://www.hapakenya.com/wp-content/uploads/2013/05/Call-of-Duty.jpg",
+  "https://www.sde.co.ke/sdemedia/sdeimages/friday/xylmxin5fm29j599ff422e9e09.jpg",];
+var gamingplacenames = [
+" Trics gaming lounge, Nairobi",
+" Gaming masters lounge",
+" Call of duty",
+" Game masters lounge, Nairobi",];
 
 
 var countrender = false;
